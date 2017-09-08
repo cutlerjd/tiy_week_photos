2fedbc66cd5649a03cf6f89598b0eaa90a5613c6
Jase Cutler <jasecutler@gmail.com>
2017-09-08 10:54:01 -0700

    Added loading albums on app load

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,25 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from '../store'
 
+import AlbumsView from './AlbumsView'
+import AlbumView from './AlbumView'
+import PhotoView from './PhotoView'
+
+import {fetchAlbums} from '../actions/photoActions'
+
 class App extends Component {
+  componentWillMount(){
+    fetchAlbums()
+    console.log()
+  }
   render() {
     return (
       <Provider store={store}>
         <Router>
           <Switch>
+          <Route exact path="/" component={AlbumsView}/>
+          <Route exact path="/album/:id" component={AlbumView}/>
+          <Route exact path="/album/:albumId/:photoId" component={PhotoView}/>
           <div className="App">
           <h2>Base Photo App</h2>
         </div>
